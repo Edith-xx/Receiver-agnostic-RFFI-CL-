17394709afbad5807b0745ad0624404e3651c245
Edith-xx <652022230002@smail.nju.edu.cn>
2025-08-15 17:09:43 +0800

    run_DIFEX.sh

--- run_DIFEX.sh
+++ run_DIFEX.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
 lams=(1.0 0.5 0.1 0.05 0.01 0.005 0.001)
-#lams=(1.0)
-
 for lam in "${lams[@]}"
 do
     echo "Running train_DIFEX1 with lam=$lam"
@@ -14,5 +11,3 @@ do
     python train_DIFEX1.py --beta 0.005 --lam "$lam"
     python train_DIFEX1.py --beta 0.001 --lam "$lam"
 done
-#左边文件夹的参数第一个是lam,第二个是beta
-#day1Rx14_Rx512_F1256_F2256里面跑了两组实验，下面一组的256_512,上面256_256
